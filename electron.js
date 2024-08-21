7def03e2fb6e639b1dae19ffaee1474d70cdcbe9
cbh778899 <cbh778899@outlook.com>
2024-08-21 18:17:31 +1000

    electron app based on react
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

---
+++ electron.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        show: false,
+        height: 600,
+        width: 900,
+        minWidth: 560,
+        minHeight: 250,
+        transparent: true,
+        autoHideMenuBar: true
+    })
+
+    win.loadURL("http://localhost:3000");
+    win.once("ready-to-show", ()=>{
+        win.show();
+    })
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('window-all-closed', () => {
+        process.platform !== 'darwin' && app.quit()
+    });
+
+    app.on('activate', () => {
+        BrowserWindow.getAllWindows().length === 0 && createWindow()
+    })
+})
