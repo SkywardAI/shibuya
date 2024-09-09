a86a47f88820a7717a1b016258599056d5523c4a
cbh778899 <cbh778899@outlook.com>
2024-09-09 13:58:17 +1000

    change the way to generate sidebar
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- src/utils/types.jsx
+++
@@ -1,9 +0,0 @@
-import { Chat, Gear } from "react-bootstrap-icons";
-
-export const sidebar_top_menus = [
-    { name: "chat", url: '/chat', icon: <Chat className="icon" /> }
-]
-
-export const sidebar_bottom_menus = [
-    { name: "settings", url: '/settings', icon: <Gear className="icon" /> }
-]
