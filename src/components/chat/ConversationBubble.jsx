24b258578fea076798a8f19939aae020b3b57d82
cbh778899 <cbh778899@outlook.com>
2024-09-10 17:34:52 +1000

    implement basic chat feature
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- src/components/chat/ConversationBubble.jsx
+++ src/components/chat/ConversationBubble.jsx
@@ -1,7 +1,18 @@
-export default function ConversationBubble({role, content}) {
+import { CircleFill } from "react-bootstrap-icons"
+import Markdown from "react-markdown"
+
+export default function ConversationBubble({role, content, hidden}) {
     return (
-        <div className={`bubble ${role}`}>
-            { content }
+        <div className={`bubble ${role}${hidden?' hidden':""}`}>
+            {
+                content ?
+                <Markdown>{ content }</Markdown> :
+                <>
+                <CircleFill className="dot-animation" />
+                <CircleFill className="dot-animation" />
+                <CircleFill className="dot-animation" />
+                </>
+            }
         </div>
     )
 }
