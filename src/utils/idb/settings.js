0bfc7c0b9132b8e563461e1f2f7ce0fc7cdd2acd
cbh778899 <cbh778899@outlook.com>
2024-09-10 17:34:02 +1000

    add wrap for indexeddb
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

---
+++ src/utils/idb/settings.js
@@ -0,0 +1,29 @@
+export const DEFAULT_TABLE_SETTINGS = { keyPath: 'id', autoIncrement: true }
+
+export const DB_NAME = 'shibuya-idb'
+
+export const versions = [
+    {
+        version: 1,
+        tables: {
+            'chat-history': {
+                op: 'new',
+                columns: [
+                    { name: 'title' },
+                    { name: 'uid' },
+                    { name: 'createdAt' },
+                    { name: 'updatedAt' },
+                ]
+            },
+            'messages': {
+                op: 'new',
+                columns: [
+                    { name: 'history-uid' },
+                    { name: 'role' },
+                    { name: 'content' },
+                    { name: 'createdAt' }
+                ]
+            }
+        }
+    }
+]
