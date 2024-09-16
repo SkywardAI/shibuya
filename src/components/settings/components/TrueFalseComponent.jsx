b6767713fb9f571f8793603449444885a4207e5f
cbh778899 <cbh778899@outlook.com>
2024-09-16 12:42:43 +1000

    update style
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- src/components/settings/components/TrueFalseComponent.jsx
+++ src/components/settings/components/TrueFalseComponent.jsx
@@ -3,8 +3,12 @@ export default function TrueFalseComponent({ cb, value, title, description }) {
         <div className="component">
             <div className="title">{title}</div>
             { description && <div className="description">{description}</div> }
-            <div className="checkbox-container main-part">
-                <input className="clickable" type="checkbox" checked={value} onChange={(evt)=>cb(evt.target.checked)} />
+            <div className="main-part checkbox">
+                <div className="text">OFF</div>
+                <div className="checkbox-container">
+                    <input className="clickable" type="checkbox" checked={value} onChange={(evt)=>cb(evt.target.checked)} />
+                </div>
+                <div className="text">ON</div>
             </div>
         </div>
     )
