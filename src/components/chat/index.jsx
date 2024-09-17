7b17040e7185b5f77f13ff69aad75b103495f75d
cbh778899 <cbh778899@outlook.com>
2024-09-17 16:46:36 +1000

    save client information for switch between conversations
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -1,15 +1,32 @@
 import { useState } from "react";
 import Tickets from "./Tickets";
 import Conversation from "./Conversation";
+import useIDB from "../../utils/idb";
 
 export default function Chat() {
 
     const [chat, selectChat] = useState({});
+    const [history, setHistory] = useState([]);
+    const idb = useIDB();
+
+    function updateChatClient(client) {
+        selectChat({
+            ...chat, client
+        })
+        
+        let history_cp = [...history];
+        history_cp[
+            history_cp.findIndex(e=>e.uid === chat.uid)
+        ].client = client;
+        setHistory(history_cp);
+
+        idb.updateOne('chat-history', {client}, [{uid:chat.uid}])
+    }
 
     return (
         <div className="chat">
-            <Tickets selectChat={selectChat} current_chat={chat} />
-            <Conversation uid={chat.uid} client={chat.client} />
+            <Tickets selectChat={selectChat} setHistory={setHistory} history={history} current_chat={chat} />
+            <Conversation uid={chat.uid} client={chat.client} updateClient={updateChatClient} />
         </div>
     )
 }
