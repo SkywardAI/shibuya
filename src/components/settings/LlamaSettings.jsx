8b0245b57856352be988bef80030ca03939723b8
cbh778899 <cbh778899@outlook.com>
2024-10-14 17:17:23 +1100

    add llama settings
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

---
+++ src/components/settings/LlamaSettings.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import SettingSection from "./SettingSection";
+import TrueFalseComponent from "./components/TrueFalseComponent";
+import TextComponent from "./components/TextComponent";
+import useIDB from "../../utils/idb";
+import { getPlatformSettings, updatePlatformSettings } from "../../utils/general_settings";
+import { DEFAULT_LLAMA_CPP_MODEL_URL } from "../../utils/types";
+
+export default function LlamaSettings({ trigger, enabled, updateEnabled, openDownloadProtector }) {
+
+    const [model_download_link, setModelDownloadLink] = useState('');
+    const idb = useIDB();
+
+    async function saveSettings() {
+        const url = model_download_link || DEFAULT_LLAMA_CPP_MODEL_URL
+        // prevent from leave model url empty
+        if(url !== model_download_link) setModelDownloadLink(url)
+
+        updatePlatformSettings({
+            llama_model_url: url
+        })
+        if(enabled) {
+            // check if model with this url already downloaded
+            let stored_model = await idb.getOne("downloaded-models", {where: [{ platform: 'Llama', url }]})
+            console.log(stored_model)
+            // if no model record, means not downloaded
+            if(!stored_model) {
+                await openDownloadProtector(
+                    "Please wait while we downloading the model...",
+                    `Downloading model from ${url}`,
+                    async callback=>{
+                        const model_info = await window['node-llama-cpp'].downloadModel(url, callback)
+                        if(model_info) {
+                            const { model_name, url, size, finish_time } = model_info;
+                            stored_model = {
+                                'model-name': model_name,
+                                url, size, createdAt: finish_time,
+                                platform: 'Llama'
+                            }
+                            await idb.insert("downloaded-models", stored_model)
+                        }
+                    }
+                )
+            }
+            // load model using the model name retrieved
+            await window['node-llama-cpp'].loadModel(stored_model['model-name'])
+        }
+    }
+
+    useEffect(()=>{
+        trigger && saveSettings();
+    // eslint-disable-next-line
+    }, [trigger])
+
+    useEffect(()=>{
+        const { llama_model_url } = getPlatformSettings();
+        setModelDownloadLink(llama_model_url || DEFAULT_LLAMA_CPP_MODEL_URL);
+    }, [])
+
+    return (
+        <SettingSection title={'Llama.cpp Engine Settings'}>
+            <TrueFalseComponent 
+                title={"Use Llama.cpp Engine For Completion"}
+                description={"Llama.cpp Engine is powerful and it allows you to run your own .gguf model using GPU on your local machine."}
+                value={enabled} cb={updateEnabled}
+            />
+            <TextComponent 
+                title={"Set the link of model you want to use"}
+                description={"Only models with extension .gguf can be used. Please make sure it can be run on your own machine."}
+                placeholder={"Default model is Microsoft Phi-3"}
+                value={model_download_link} cb={setModelDownloadLink}
+            />
+        </SettingSection>
+    )
+}
--- src/components/settings/index.jsx
+++ src/components/settings/index.jsx
@@ -4,11 +4,19 @@ import { getPlatformSettings, updatePlatformSettings } from "../../utils/general
 import ModelSettings from "./ModelSettings";
 import OpenaiSettings from "./OpenaiSettings";
 import WllamaSettings from "./WllamaSettings";
+import DownloadProtector from "./DownloadProtector";
+import LlamaSettings from "./LlamaSettings";
 
-export default function Settings() {
+export default function Settings({ complete }) {
 
     const [enabled_platform, setEnabledPlatform] = useState(getPlatformSettings().enabled_platform)
     const [ saveSettingTrigger, toggleSaveSetting ] = useState(false);
+
+    // Download model dialog
+    const [download_title, setDownloadTitle] = useState('');
+    const [download_spec, setDownloadSpec] = useState('');
+    const [protector_open_status, toggleProtectorOpenStatus] = useState(false);
+    const [download_progress, setDownloadProgress] = useState(0);
 
     function updatePlatform(platform = null) {
         setEnabledPlatform(platform);
@@ -18,6 +26,20 @@ export default function Settings() {
     function save() {
         toggleSaveSetting(true);
         setTimeout(()=>toggleSaveSetting(false), 1000);
+        complete && complete();
+    }
+
+    async function openDownloadProtector(title, description, downloader) {
+        setDownloadTitle(title);
+        setDownloadSpec(description);
+        toggleProtectorOpenStatus(true);
+
+        await downloader((progress, is_finished)=>{
+            setDownloadProgress(progress);
+            if(is_finished) {
+                toggleProtectorOpenStatus(false);
+            }
+        })
     }
 
     return (
@@ -25,9 +47,17 @@ export default function Settings() {
             <ModelSettings 
                 trigger={saveSettingTrigger}
             />
+            <LlamaSettings 
+                trigger={saveSettingTrigger}
+                enabled={enabled_platform === "Llama"}
+                updateEnabled={set=>updatePlatform(set ? "Llama" : null)}
+                openDownloadProtector={openDownloadProtector}
+            />
             <WllamaSettings 
                 trigger={saveSettingTrigger}
-                enabled={!enabled_platform}
+                enabled={enabled_platform === "Wllama"}
+                updateEnabled={set=>updatePlatform(set ? "Wllama" : null)}
+                openDownloadProtector={openDownloadProtector}
             />
             <AwsSettings 
                 trigger={saveSettingTrigger}
@@ -42,6 +72,10 @@ export default function Settings() {
             <div className={`save-settings clickable${saveSettingTrigger?" saved":""}`} onClick={save}>
                 { saveSettingTrigger ? "Settings Saved!" : "Save Settings" }
             </div>
+            <DownloadProtector 
+                title={download_title} description={download_spec} 
+                progress={download_progress} open_status={protector_open_status} 
+            />
         </div>
     )
 }
