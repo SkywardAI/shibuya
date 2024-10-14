edd0efd6b0f251df9e7f38254a23392496488e19
cbh778899 <cbh778899@outlook.com>
2024-10-14 17:19:43 +1100

    add default url for download phi3
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- src/utils/types.js
+++ src/utils/types.js
@@ -5,3 +5,5 @@ export const LOAD_FIRST_TIME = 2;
 export const LOAD_FINISHED = 1;
 export const LOAD_SET_SETTINGS = 2;
 export const LOAD_SKIP_SETTINGS = 3;
+
+export const DEFAULT_LLAMA_CPP_MODEL_URL = "https://huggingface.co/aisuko/Phi-3-mini-4k-instruct-gguf/resolve/main/Phi3-mini-4k-instruct-Q4.gguf"
